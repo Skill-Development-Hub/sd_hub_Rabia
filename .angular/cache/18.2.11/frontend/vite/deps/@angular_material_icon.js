fc61c02cc031d9578440c54a4af8ae6fb05695a4
rabia79872 <rabiaali2050@gmail.com>
2024-11-07 13:13:02 +0530

    New app

---
+++ .angular/cache/18.2.11/frontend/vite/deps/@angular_material_icon.js
@@ -0,0 +1,34 @@
+import {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+} from "./chunk-IQASY52L.js";
+import "./chunk-ISBPOKCC.js";
+import "./chunk-RAX7NWH6.js";
+import "./chunk-7EXB5PIH.js";
+import "./chunk-CG3332AW.js";
+import "./chunk-4LY645BM.js";
+export {
+  ICON_REGISTRY_PROVIDER,
+  ICON_REGISTRY_PROVIDER_FACTORY,
+  MAT_ICON_DEFAULT_OPTIONS,
+  MAT_ICON_LOCATION,
+  MAT_ICON_LOCATION_FACTORY,
+  MatIcon,
+  MatIconModule,
+  MatIconRegistry,
+  getMatIconFailedToSanitizeLiteralError,
+  getMatIconFailedToSanitizeUrlError,
+  getMatIconNameNotFoundError,
+  getMatIconNoHttpProviderError
+};
+//# sourceMappingURL=@angular_material_icon.js.map
